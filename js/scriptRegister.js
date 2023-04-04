da4d300a4b9d5b9ce991dc2ed9a3bf9ebea43ef9
segea17 <segea86@gmail.com>
2023-04-04 19:07:54 -0300

    Cambios realizados en el CSS del formulario y links imagenes

--- js/scriptRegister.js
+++ js/scriptRegister.js
@@ -1,12 +1,17 @@
-//Debemos llamar a nuestro elemento formulario desde html y traerlo a JS
 const formularioUsuarios = document.querySelector(".formUsuarios")
-//Lo llamé por clase en vez de por id, por eso usé el querySelector. De tener varias clases llamadas iguales, siempre va a llamar a la primera.
-//Personalmente al profesor prefiere llamar por id, pero hay gente que usa seleccionar por clases.
-const usuarios = []
 const usuariosContainerHTML = document.querySelector("#usuariosContainer")
-//Es más nuevo el querySelector y por eso está más de moda. Pero al profesor no le gustó mucho.
-formularioUsuarios.addEventListener("submit", (event) => {
-    event.preventDefault() //Para evitar el comportamiento por defecto del submit, que se recarga solo
+/* document.getElementsByClassName("inputContainer") / document.querySelectorAll(".inputContainer") */
+
+const usuarios = []
+
+
+formularioUsuarios.addEventListener("submit", (event) =>{
+    event.preventDefault()
     console.log(formularioUsuarios.provincia.value)
-    usuarios.push(formularioUsuarios.provincia.value)
+    usuarios.push({
+        nombre: formularioUsuarios.nombre.value,
+        email: formularioUsuarios.email.value,
+        contrasena: formularioUsuarios.contrasena.value,
+        provincia:formularioUsuarios.provincia.value
+    })
 })
