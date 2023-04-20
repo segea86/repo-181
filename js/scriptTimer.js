f9cfd0bcb2c122b95ca2be5a05342a4113ae6880
segea17 <segea86@gmail.com>
2023-04-20 17:08:48 -0300

    style final

--- js/scriptTimer.js
+++ js/scriptTimer.js
@@ -1,11 +1,11 @@
-var fechaLimite = new Date("April 30, 2023 00:00:00").getTime();
-var x = setInterval(function() {
-    var ahora = new Date().getTime();
-    var tiempoRestante = fechaLimite - ahora;
-    var dias = Math.floor(tiempoRestante/(1000*60*60*24))
-    var horas = Math.floor((tiempoRestante % (1000*60*60*24))/(1000*60*60));
-    var minutos = Math.floor((tiempoRestante % (1000*60*60))/(1000*60));
-    var segundos = Math.floor((tiempoRestante % (1000*60))/(1000));
+const fechaLimite = new Date("May 1, 2023 00:00:00").getTime();
+const x = setInterval(()=> {
+    const ahora = new Date().getTime();
+    const tiempoRestante = fechaLimite - ahora;
+    const dias = Math.floor(tiempoRestante/(1000*60*60*24))
+    const horas = Math.floor((tiempoRestante % (1000*60*60*24))/(1000*60*60));
+    const minutos = Math.floor((tiempoRestante % (1000*60*60))/(1000*60));
+    const segundos = Math.floor((tiempoRestante % (1000*60))/(1000));
 document.getElementById("timer").innerHTML= dias + " días " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 if (tiempoRestante<0){
     clearInterval(x);
